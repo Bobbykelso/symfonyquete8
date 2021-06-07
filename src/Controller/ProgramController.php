b4a1aada1363be950a60225e8752459839888868
Bobbykelso <barth.aurelien182@gmail.com>
2021-06-07 20:44:53 +0200

    first commit

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,37 @@
+<?php
+
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProgramController
+ * @Route("/programs", name="program_")
+ */
+Class ProgramController extends AbstractController
+{
+    /**
+     * Correspond à la route /programs/ et au name "program_index"
+     * @Route("/", name="program_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * Correspond à la route /programs/{id} et au name "program_show"
+     * @Route("/{id}/", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+     */
+    public function show(int $id = 4): Response
+    {
+        return $this->render('Program/show.html.twig', [
+            'id' => $id
+        ]);
+    }
+}
